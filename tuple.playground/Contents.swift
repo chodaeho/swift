3be04a0fa9d3c7dbedda3f4aad282a55aad6c526
daehocho <daehocho0637@gmail.com>
2021-10-04 23:06:23 +0900

    swift 문법 튜플

--- tuple.playground/Contents.swift
+++ tuple.playground/Contents.swift
@@ -1,6 +1,19 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// --- Tuple ---
+let coordinates = (4, 6)
 
+let x = coordinates.0
+let y = coordinates.1
 
-print("test")
+//다음은 튜플을 이용헤 위의 코드보다 명시적으로 표현 가능함
+let coordinatesNamed = (x: 2, y: 5)
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+//다음은 위의 코드를 한 줄로 표현 가능함
+let (x3, y3) = coordinatesNamed
+
+x3
+y3
