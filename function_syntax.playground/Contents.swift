d4320b3654de0eea13a640f653f1738042602e62
daehocho <daehocho0637@gmail.com>
2021-10-09 12:53:04 +0900

    function 구문 두번째 커밋

--- function_syntax.playground/Contents.swift
+++ function_syntax.playground/Contents.swift
@@ -24,3 +24,25 @@ func incrementAndPrint(_ value: inout Int) {
     print(value)
 }
 incrementAndPrint(&value)
+
+
+// --- Function as a param
+// 함수를 변수에 할당 할 수 있음.
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+func subtract(_ a: Int, _ b: Int) -> Int {
+    return a - b
+}
+var function = add
+function(4, 2)
+function = subtract
+function(4, 2)
+
+// 함수를 파라미터로 받아서 사용할 수 있음
+func printResult(_ function: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    let result = function(a, b)
+    print(result)
+}
+printResult(add, 10, 5)
+printResult(subtract, 10, 5)
